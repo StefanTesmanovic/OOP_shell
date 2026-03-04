aa5bfe99895ea3fbdc6dbd3acacc50ab8333d31b
Stesman <tesmanovic166@gmail.com>
2026-03-04 20:28:45 +0100

    nema ovde nista

---
+++ build/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stesman/Documents/coding/ProjOOP/Commands/Command.cpp" "CMakeFiles/untitled.dir/Commands/Command.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/Command.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/DateCommand.cpp" "CMakeFiles/untitled.dir/Commands/DateCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/DateCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/EchoCommand.cpp" "CMakeFiles/untitled.dir/Commands/EchoCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/EchoCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/Pipe.cpp" "CMakeFiles/untitled.dir/Commands/Pipe.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/Pipe.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/PromptCommand.cpp" "CMakeFiles/untitled.dir/Commands/PromptCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/PromptCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/RmCommand.cpp" "CMakeFiles/untitled.dir/Commands/RmCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/RmCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/TimeCommand.cpp" "CMakeFiles/untitled.dir/Commands/TimeCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/TimeCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/TouchCommand.cpp" "CMakeFiles/untitled.dir/Commands/TouchCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/TouchCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/TruncateCommand.cpp" "CMakeFiles/untitled.dir/Commands/TruncateCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/TruncateCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Commands/WcCommand.cpp" "CMakeFiles/untitled.dir/Commands/WcCommand.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands/WcCommand.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Processing/CommandFactory.cpp" "CMakeFiles/untitled.dir/Processing/CommandFactory.cpp.o" "gcc" "CMakeFiles/untitled.dir/Processing/CommandFactory.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Processing/InputParser.cpp" "CMakeFiles/untitled.dir/Processing/InputParser.cpp.o" "gcc" "CMakeFiles/untitled.dir/Processing/InputParser.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/Processing/Shell.cpp" "CMakeFiles/untitled.dir/Processing/Shell.cpp.o" "gcc" "CMakeFiles/untitled.dir/Processing/Shell.cpp.o.d"
+  "/home/stesman/Documents/coding/ProjOOP/main.cpp" "CMakeFiles/untitled.dir/main.cpp.o" "gcc" "CMakeFiles/untitled.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/Commands/Command.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/Command.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/DateCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/DateCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/EchoCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/EchoCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/Pipe.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/Pipe.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/PromptCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/PromptCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/RmCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/RmCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/TimeCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/TimeCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/TouchCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/TouchCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/TruncateCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/TruncateCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Commands/WcCommand.cpp.o"
+  "CMakeFiles/untitled.dir/Commands/WcCommand.cpp.o.d"
+  "CMakeFiles/untitled.dir/Processing/CommandFactory.cpp.o"
+  "CMakeFiles/untitled.dir/Processing/CommandFactory.cpp.o.d"
+  "CMakeFiles/untitled.dir/Processing/InputParser.cpp.o"
+  "CMakeFiles/untitled.dir/Processing/InputParser.cpp.o.d"
+  "CMakeFiles/untitled.dir/Processing/Shell.cpp.o"
+  "CMakeFiles/untitled.dir/Processing/Shell.cpp.o.d"
+  "CMakeFiles/untitled.dir/main.cpp.o"
+  "CMakeFiles/untitled.dir/main.cpp.o.d"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
